b3af4b8ae585d7ac8789f19a17d6d78f3ea5a46f
Jony Devnath <76802139+jonydevnath@users.noreply.github.com>
2024-08-22 02:14:34 +0600

    Update models.py

--- store/models.py
+++ store/models.py
@@ -28,7 +28,8 @@ class Product(models.Model):
     price = models.DecimalField(default= 0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
-    image =  models.ImageField(upload_to='uploads/upload/')   
+    # image =  models.ImageField(upload_to='uploads/upload/')   
+    image_url = models.URLField(max_length=200, default='https://github.com/jonydevnath/bookrevive/blob/main/media/uploads/upload/default.png?raw=true')
     # Add Sale Stuff 
     is_sale = models.BooleanField(default=False)
     sale_price = models.DecimalField(default= 0, decimal_places=2, max_digits=6)
